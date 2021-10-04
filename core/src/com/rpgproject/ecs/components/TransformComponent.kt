92d65967767bec5ead29ef97c20f4fd24925a048
Maciej <gorski.maciej@o2.pl>
2021-10-04 23:35:39 +0200

    added position to TransformComponent.kt, added entity comparator, started implementation of render system

--- core/src/com/rpgproject/ecs/components/TransformComponent.kt
+++ core/src/com/rpgproject/ecs/components/TransformComponent.kt
@@ -1,6 +1,8 @@
 package com.rpgproject.ecs.components
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.math.Vector3
 
 class TransformComponent : Component {
+    val position = Vector3()
 }
--- core/src/com/rpgproject/ecs/systems/RenderSystem.kt
+++ core/src/com/rpgproject/ecs/systems/RenderSystem.kt
@@ -1,4 +1,17 @@
 package com.rpgproject.ecs.systems
 
-class RenderSystem {//this should be a normal processing system
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.core.Family
+import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.ashley.systems.SortedIteratingSystem
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.rpgproject.ecs.components.TextureComponent
+import com.rpgproject.ecs.components.TransformComponent
+import com.rpgproject.util.ecs.EntityComparator
+
+//this should be a normal processing system
+class RenderSystem(val spriteBatch: SpriteBatch, val renderCamera: Camera) : SortedIteratingSystem(Family.all(TextureComponent::class.java, TransformComponent::class.java).get(), EntityComparator()) {
+    override fun processEntity(entity: Entity?, deltaTime: Float) {
+    }
 }
