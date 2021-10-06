fde1afb3795aa5959d0ec8ffad7d44543ec31c38
Maciej <gorski.maciej@o2.pl>
2021-10-07 00:04:02 +0200

    added missing process() call to ECSWorld

--- core/src/com/rpgproject/RPGProjectGame.kt
+++ core/src/com/rpgproject/RPGProjectGame.kt
@@ -6,12 +6,15 @@ import com.artemis.managers.WorldSerializationManager
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.rpgproject.ecs.systems.CollisionHandlingSystem
 import com.rpgproject.ecs.systems.PhysicsSystem
 import com.rpgproject.ecs.systems.RenderSystem
+import com.rpgproject.screens.GameScreen
 import com.rpgproject.screens.MainMenuScreen
 import com.rpgproject.util.EcsWorld
 import com.rpgproject.util.PhysicsWorld
@@ -28,34 +31,40 @@ class RPGProjectGame : KtxGame<Screen>() {
     private lateinit var physicsWorld: PhysicsWorld
     private lateinit var mainCamera: OrthographicCamera
     private lateinit var serializationManager: WorldSerializationManager
+    private lateinit var assetManager: AssetManager
+    private lateinit var eventBus: EventSystem
 
     companion object {
-        lateinit var GameInstance: RPGProjectGame
-        lateinit var EventBus: EventSystem
+        lateinit var Instance: RPGProjectGame
     }
 
     override fun create() {
-        GameInstance = this
+        Instance = this
+        assetManager = AssetManager().apply { load("Jester.png", Texture::class.java) }.apply { finishLoading() }
         batch = SpriteBatch()
         mainCamera = OrthographicCamera(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
         physicsWorld = PhysicsWorld(Vector2(0f, -10f), false)
         initArtemis()
-        physicsWorld.setContactListener(CollisionListener(ecsWorld.getSystem(EventSystem::class.java)))
+        physicsWorld.setContactListener(CollisionListener(eventBus))
 
 
         addScreen(MainMenuScreen())
+        addScreen(GameScreen(ecsWorld))
         setScreen<MainMenuScreen>()
     }
 
     override fun render() {
         clearScreen(0f, 0f, 0f)
+        mainCamera.update()
+        ecsWorld.process()
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit()
             exitProcess(0)
         }
 //        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+//            println("henlo")
 //            val e = ecsWorld.createEntity()
-//            ecsWorld.getSystem(EventSystem::class.java).dispatch(object: EntityEvent(e) {})
+//            ecsWorld.edit(e.id).add(TransformComponent()).add(TextureComponent().apply { texture=assetManager.get("Jester.png", Texture::class.java) })
 //        }
         currentScreen.render(Gdx.graphics.deltaTime)
     }
@@ -64,6 +73,7 @@ class RPGProjectGame : KtxGame<Screen>() {
         batch.dispose()
         ecsWorld.dispose()
         physicsWorld.dispose()
+        assetManager.dispose()
     }
 
     private fun initArtemis() {
@@ -74,8 +84,8 @@ class RPGProjectGame : KtxGame<Screen>() {
                         RenderSystem(batch, mainCamera),
                         CollisionHandlingSystem())
                 .build()
-        EventBus = EventSystem()
-        config.setSystem(EventBus)
+        eventBus = EventSystem()
+        config.setSystem(eventBus)
         config.setSystem(serializationManager)
         ecsWorld = EcsWorld(config)
         serializationManager.setSerializer(JsonArtemisSerializer(ecsWorld))
--- core/src/com/rpgproject/ecs/systems/RenderSystem.kt
+++ core/src/com/rpgproject/ecs/systems/RenderSystem.kt
@@ -1,37 +1,35 @@
 package com.rpgproject.ecs.systems
 
 import com.artemis.ComponentMapper
-import com.artemis.Entity
 import com.artemis.annotations.All
 import com.artemis.annotations.SkipWire
-import com.artemis.systems.EntityProcessingSystem
+import com.artemis.systems.IteratingSystem
 import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.rpgproject.ecs.components.TextureComponent
 import com.rpgproject.ecs.components.TransformComponent
 
 @All(TransformComponent::class, TextureComponent::class)
-class RenderSystem(private val spriteBatch: SpriteBatch, private val camera: Camera) : EntityProcessingSystem() {
+class RenderSystem(private val spriteBatch: SpriteBatch, private val camera: Camera) : IteratingSystem() {
 
     private lateinit var transformMapper: ComponentMapper<TransformComponent>
     private lateinit var textureMapper: ComponentMapper<TextureComponent>
 
     @SkipWire
-    private var entityComparator: Comparator<Entity> = Comparator { o1, o2 ->
+    private var entityComparator: Comparator<Int> = Comparator { o1, o2 ->
         val t1 = transformMapper.get(o1)
         val t2 = transformMapper.get(o2)
         t1.position.z.compareTo(t2.position.z)
     }
 
     override fun begin() {
-        entities.sort(entityComparator)
+        spriteBatch.projectionMatrix = camera.combined
         spriteBatch.begin()
     }
 
-    override fun process(e: Entity) {
-        //draw entities heeeere
-        val transform = transformMapper.get(e)
-        val texture = textureMapper.get(e)
+    override fun process(entityId: Int) {
+        val transform = transformMapper.get(entityId)
+        val texture = textureMapper.get(entityId)
         if (texture.texture == null) return
         spriteBatch.draw(texture.texture, transform.position.x, transform.position.y)
     }
