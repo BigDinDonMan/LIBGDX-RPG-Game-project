3725f28c2b0b87820d963ea4d126f7e7319606a2
Maciej <gorski.maciej@o2.pl>
2021-10-26 11:53:06 +0200

    fixed a bug with overriding look direction vector with 0 when pressing opposite keys quickly

--- core/src/com/rpgproject/input/KeyboardHandler.kt
+++ core/src/com/rpgproject/input/KeyboardHandler.kt
@@ -1,6 +1,7 @@
 package com.rpgproject.input
 
 import com.artemis.Entity
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.rpgproject.ecs.events.specific.PlayerInputEvent
 import ktx.app.KtxInputAdapter
@@ -12,7 +13,7 @@ class KeyboardHandler(val playerEntity: Entity, val eventSystem: EventSystem) : 
     private var y = 0f
 
     override fun keyDown(keycode: Int): Boolean {
-        when (keycode) { //todo: handle the case when you quickly stop pressing the key and quickly press the one with opposite value
+        when (keycode) {
             Input.Keys.W -> y = 1f
             Input.Keys.S -> y = -1f
             Input.Keys.A -> x = -1f
@@ -25,11 +26,11 @@ class KeyboardHandler(val playerEntity: Entity, val eventSystem: EventSystem) : 
     }
 
     override fun keyUp(keycode: Int): Boolean {
-        when (keycode) { //todo: dont set params to 0 when the other key is pressed
-            Input.Keys.W -> y = 0f
-            Input.Keys.S -> y = 0f
-            Input.Keys.A -> x = 0f
-            Input.Keys.D -> x = 0f
+        when (keycode) {
+            Input.Keys.W -> y = if (Gdx.input.isKeyPressed(Input.Keys.S)) y else 0f
+            Input.Keys.S -> y = if (Gdx.input.isKeyPressed(Input.Keys.W)) y else 0f
+            Input.Keys.A -> x = if (Gdx.input.isKeyPressed(Input.Keys.D)) x else 0f
+            Input.Keys.D -> x = if (Gdx.input.isKeyPressed(Input.Keys.A)) x else 0f
         }
 
         eventSystem.dispatch(PlayerInputEvent(playerEntity, x, y))
