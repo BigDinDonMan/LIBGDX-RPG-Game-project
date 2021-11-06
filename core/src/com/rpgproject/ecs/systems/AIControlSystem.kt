0f1cc7ab991809a04e6ef7b8d461e0bc800fe52e
Maciej <gorski.maciej@o2.pl>
2021-11-06 15:10:59 +0100

    added empty components and systems for AI and Enemies

---
+++ core/src/com/rpgproject/ecs/systems/AIControlSystem.kt
@@ -0,0 +1,9 @@
+package com.rpgproject.ecs.systems
+
+import com.artemis.systems.IteratingSystem
+
+class AIControlSystem : IteratingSystem() {
+    override fun process(entityId: Int) {
+
+    }
+}
