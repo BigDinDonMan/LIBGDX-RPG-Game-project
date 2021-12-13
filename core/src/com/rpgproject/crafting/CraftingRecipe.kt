a00a9c4ffcbdfd078c08dc5b3c8572909c4ab596
BigDinDonMan <gorski.maciej@o2.pl>
2021-12-13 20:14:13 +0100

    WIP crafting

--- core/src/com/rpgproject/crafting/CraftingRecipe.kt
+++ core/src/com/rpgproject/crafting/CraftingRecipe.kt
@@ -2,6 +2,7 @@ package com.rpgproject.crafting
 
 import com.rpgproject.inventory.InventoryItem
 
-class CraftingRecipe {
+class CraftingRecipe(val outputItem: InventoryItem) {
     private val materials: HashMap<InventoryItem, Int> = HashMap()
+    private val items: HashMap<InventoryItem, Int> = HashMap()
 }
